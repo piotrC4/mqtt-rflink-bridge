ffe3a82bc4034b44bc090f40cca3e4311c1cb03c
enc-X <chodor+github@gmail.com>
2019-11-03 11:11:02 +0100

    migration to vs code and homie v.2 dev

--- src/main.cpp
+++ src/main.cpp
@@ -45,9 +45,9 @@ int lastButtonValue = 1; // previous state of build-in button
 // Functions delcaration
 void loopHandler(); // Homie loop handler
 void setupHandler(); // Homie setup handler
-void onHomieEvent(HomieEvent event); // Homie event handler
-bool serialMessageHandler(String message); // Serial message hander
-bool publishModeHandler(String message); // Method of publishing
+void HomieEventHandler(const HomieEvent& event); // Homie event handler
+bool serialMessageHandler(const HomieRange& range, const String& message); // Serial message hander
+bool publishModeHandler(const HomieRange& range, const String& message); // Method of publishing
 
 /***************************************************************
  * Main setup - Homie initialization
@@ -75,25 +75,22 @@ void setup() {
   mySerial.begin(57600);
 
   /* Initiate homie object */
-  Homie.setFirmware(FIRMWARE_NAME, FIRMWARE_VERSION);
+  Homie_setFirmware(FIRMWARE_NAME, FIRMWARE_VERSION);
 
   Homie.setLedPin(PIN_LED_RED, HIGH);
   Homie.setResetTrigger(PIN_BUTTON, LOW, 10000);
 
-  serialNode.subscribe("to-send", serialMessageHandler);
-  serialNode.subscribe("publish-mode", publishModeHandler);
+  serialNode.advertise("to-send").settable(serialMessageHandler);
+  serialNode.advertise("publish-mode").settable(publishModeHandler);
 
   Homie.setSetupFunction(setupHandler);
   Homie.setLoopFunction(loopHandler);
 
-#ifdef DEBUG
-  Homie.enableLogging(true);
-#else
-  Homie.enableLogging(false);
+#ifndef DEBUG
+  Homie.disableLogging();
 #endif
 
-  Homie.registerNode(serialNode);
-  Homie.onEvent(onHomieEvent);
+  Homie.onEvent(HomieEventHandler);
   Homie.setup();
 
 }
@@ -112,36 +109,106 @@ void loop()
 /***************************************************************
  * Homie event
  */
-void onHomieEvent(HomieEvent event) {
-  switch(event) {
-    case HOMIE_CONFIGURATION_MODE:
+void HomieEventHandler(const HomieEvent& event) 
+{
+  switch(event.type) {
+    case HomieEventType::CONFIGURATION_MODE: // Default eeprom data in configuration mode
       // Configuration mode is started
       EEpromData.publishMode = MODE_STANDARD;
       EEPROM.put(0, EEpromData);
       EEPROM.commit();
       break;
-    case HOMIE_NORMAL_MODE:
-      // Normal mode is started
+    case HomieEventType::STANDALONE_MODE:
+      // Do whatever you want when standalone mode is started
       break;
-    case HOMIE_OTA_MODE:
-      // OTA mode is started
+   case HomieEventType::NORMAL_MODE:
+      // Do whatever you want when normal mode is started
       break;
-    case HOMIE_ABOUT_TO_RESET:
-      // The device is about to reset
+    case HomieEventType::OTA_STARTED:
+      // Do whatever you want when OTA is started
       break;
-    case HOMIE_WIFI_CONNECTED:
-      // Wi-Fi is connected in normal mode - force reboot
+    case HomieEventType::OTA_PROGRESS:
+      // Do whatever you want when OTA is in progress
+
+      // You can use event.sizeDone and event.sizeTotal
       break;
-    case HOMIE_WIFI_DISCONNECTED:
-      // Wi-Fi is disconnected in normal mode - reboot to defaults
-      // ESP.restart();
+    case HomieEventType::OTA_FAILED:
+      // Do whatever you want when OTA is failed
       break;
-    case HOMIE_MQTT_CONNECTED:
-      // MQTT is connected in normal mode
+    case HomieEventType::OTA_SUCCESSFUL:
+      // Do whatever you want when OTA is successful
       break;
-    case HOMIE_MQTT_DISCONNECTED:
-      // MQTT is disconnected in normal mode - force reboot
-      // ESP.restart();
+    case HomieEventType::ABOUT_TO_RESET:
+      // Do whatever you want when the device is about to reset
+      break;
+    case HomieEventType::WIFI_CONNECTED:
+      // Do whatever you want when Wi-Fi is connected in normal mode
+
+      // You can use event.ip, event.gateway, event.mask
+      break;
+    case HomieEventType::WIFI_DISCONNECTED:
+      // Do whatever you want when Wi-Fi is disconnected in normal mode
+
+      // You can use event.wifiReason
+      /*
+        Wi-Fi Reason (souce: https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/WiFiClientEvents/WiFiClientEvents.ino)
+        0  SYSTEM_EVENT_WIFI_READY               < ESP32 WiFi ready
+        1  SYSTEM_EVENT_SCAN_DONE                < ESP32 finish scanning AP
+        2  SYSTEM_EVENT_STA_START                < ESP32 station start
+        3  SYSTEM_EVENT_STA_STOP                 < ESP32 station stop
+        4  SYSTEM_EVENT_STA_CONNECTED            < ESP32 station connected to AP
+        5  SYSTEM_EVENT_STA_DISCONNECTED         < ESP32 station disconnected from AP
+        6  SYSTEM_EVENT_STA_AUTHMODE_CHANGE      < the auth mode of AP connected by ESP32 station changed
+        7  SYSTEM_EVENT_STA_GOT_IP               < ESP32 station got IP from connected AP
+        8  SYSTEM_EVENT_STA_LOST_IP              < ESP32 station lost IP and the IP is reset to 0
+        9  SYSTEM_EVENT_STA_WPS_ER_SUCCESS       < ESP32 station wps succeeds in enrollee mode
+        10 SYSTEM_EVENT_STA_WPS_ER_FAILED        < ESP32 station wps fails in enrollee mode
+        11 SYSTEM_EVENT_STA_WPS_ER_TIMEOUT       < ESP32 station wps timeout in enrollee mode
+        12 SYSTEM_EVENT_STA_WPS_ER_PIN           < ESP32 station wps pin code in enrollee mode
+        13 SYSTEM_EVENT_AP_START                 < ESP32 soft-AP start
+        14 SYSTEM_EVENT_AP_STOP                  < ESP32 soft-AP stop
+        15 SYSTEM_EVENT_AP_STACONNECTED          < a station connected to ESP32 soft-AP
+        16 SYSTEM_EVENT_AP_STADISCONNECTED       < a station disconnected from ESP32 soft-AP
+        17 SYSTEM_EVENT_AP_STAIPASSIGNED         < ESP32 soft-AP assign an IP to a connected station
+        18 SYSTEM_EVENT_AP_PROBEREQRECVED        < Receive probe request packet in soft-AP interface
+        19 SYSTEM_EVENT_GOT_IP6                  < ESP32 station or ap or ethernet interface v6IP addr is preferred
+        20 SYSTEM_EVENT_ETH_START                < ESP32 ethernet start
+        21 SYSTEM_EVENT_ETH_STOP                 < ESP32 ethernet stop
+        22 SYSTEM_EVENT_ETH_CONNECTED            < ESP32 ethernet phy link up
+        23 SYSTEM_EVENT_ETH_DISCONNECTED         < ESP32 ethernet phy link down
+        24 SYSTEM_EVENT_ETH_GOT_IP               < ESP32 ethernet got IP from connected AP
+        25 SYSTEM_EVENT_MAX
+      */
+      break;
+    case HomieEventType::MQTT_READY:
+      // Do whatever you want when MQTT is connected in normal mode
+      break;
+    case HomieEventType::MQTT_DISCONNECTED:
+      // Do whatever you want when MQTT is disconnected in normal mode
+
+      // You can use event.mqttReason
+      /*
+        MQTT Reason (source: https://github.com/marvinroger/async-mqtt-client/blob/master/src/AsyncMqttClient/DisconnectReasons.hpp)
+        0 TCP_DISCONNECTED
+        1 MQTT_UNACCEPTABLE_PROTOCOL_VERSION
+        2 MQTT_IDENTIFIER_REJECTED
+        3 MQTT_SERVER_UNAVAILABLE
+        4 MQTT_MALFORMED_CREDENTIALS
+        5 MQTT_NOT_AUTHORIZED
+        6 ESP8266_NOT_ENOUGH_SPACE
+        7 TLS_BAD_FINGERPRINT
+      */
+      break;
+    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
+      // Do whatever you want when an MQTT packet with QoS > 0 is acknowledged by the broker
+
+      // You can use event.packetId
+      break;
+    case HomieEventType::READY_TO_SLEEP:
+      // After you've called `prepareToSleep()`, the event is triggered when MQTT is disconnected
+      break;
+    case HomieEventType::SENDING_STATISTICS:
+      // Do whatever you want when statistics are sent in normal mode
       break;
   }
 }
@@ -159,7 +226,7 @@ void loopHandler()
     if (buttonValue == 0)
     {
       // Button pressed
-      Homie.setNodeProperty(serialNode, "button","pressed",false);
+      serialNode.setProperty( "button").send("pressed");
     }
   }
   debouncerButton.update();
@@ -175,7 +242,7 @@ void loopHandler()
 #endif
     int startIdxEOL = 0;
     int stopIdxEOL=0;
-    while (stopIdxEOL!=msgFromRF.length())
+    while (stopIdxEOL!=(int)msgFromRF.length())
     {
       stopIdxEOL=msgFromRF.indexOf('\n',startIdxEOL);
       if (stopIdxEOL==-1)
@@ -247,12 +314,12 @@ void loopHandler()
           String outMessage;
           root.printTo(outMessage);
 
-          publishStatus = Homie.setNodeProperty(serialNode,"JSONmsg",outMessage,false);
+          publishStatus = serialNode.setProperty("JSONmsg").send(outMessage);
         } else if (tmpPublishMode == MODE_RAW) {
 #ifdef DEBUG
           Serial.println(messageSingleRow);
 #endif
-          publishStatus = Homie.setNodeProperty(serialNode,"rawmsg",messageSingleRow,false);
+          publishStatus = serialNode.setProperty("rawmsg").send(messageSingleRow);
         } else {
           // Defeult publish method - topic path
           message01.remove(message01.length()-1);
@@ -296,13 +363,13 @@ void loopHandler()
           Serial.println("Publish to: "+newTopic+" -> "+outMessage);
 #endif
 
-          publishStatus = Homie.setNodeProperty(serialNode,newTopic,outMessage,false);
+          publishStatus = serialNode.setProperty(newTopic).send(outMessage);
         }
         if (!publishStatus)
-          Homie.setNodeProperty(serialNode,"error", "failed to pubish " + deviceName + ", message too long", false);
+          serialNode.setProperty("error").send("failed to pubish " + deviceName + ", message too long");
 
       } else {
-        Homie.setNodeProperty(serialNode,"unsupported",msgFromRF, false);
+        serialNode.setProperty("unsupported").send(msgFromRF);
       }
 // --- end of row processing
     }
@@ -314,13 +381,13 @@ void loopHandler()
  */
 void setupHandler()
 {
-  Homie.setNodeProperty(serialNode,"publish-mode", String(EEpromData.publishMode));
+  serialNode.setProperty("publish-mode").send(String(EEpromData.publishMode));
   lastButtonValue = digitalRead(PIN_BUTTON);
 }
 /****************************************************************
  * publish Method Handler
  */
-bool publishModeHandler(String message)
+bool publishModeHandler(const HomieRange& range, const String& message)
 {
   int currentMode = EEpromData.publishMode;
   if (message == "JSON" || message == String(MODE_JSON))
@@ -335,13 +402,14 @@ bool publishModeHandler(String message)
   {
     EEPROM.put(0, EEpromData);
     EEPROM.commit();
-    Homie.setNodeProperty(serialNode,"publish-mode", String(EEpromData.publishMode));
+    serialNode.setProperty("publish-mode").send(String(EEpromData.publishMode));
   }
+  return true;
 }
 /****************************************************************
  * Serial message handler
  */
-bool serialMessageHandler(String value)
+bool serialMessageHandler(const HomieRange& range, const String& value)
 {
 #ifdef DEBUG
    Serial.print("Send to RF:");
